b653d95564962b5a402c0ecd543884af7d28c3c0
Moreene <moreenewu@gmail.com>
2024-10-26 01:43:27 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>這是 index 頁</h2>
+    <nuxt-link to="/about">about 頁</nuxt-link><br>
+    <nuxt-link to="/category/666">category 參數頁</nuxt-link><br>
+    <Test />
+  </div>
+</template>
